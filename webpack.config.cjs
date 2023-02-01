f8c4b027508000e2343000ecb0e7274f506a2365
jiangfan <lostwind233@outlook.com>
2023-02-01 21:37:59 +0800

    commit

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -36,7 +36,7 @@ module.exports = {
   ],
 
   output: {
-    path: path.resolve(__dirname, "doc"),
+    path: path.resolve(__dirname, "docs"),
     filename: "[name].bundle.js",
     chunkFilename: "[name].chunk.js",
     clean: true,
