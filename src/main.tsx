a60c593c671bdef0b40fd743da04b35ffe84c92e
jiangfan <lostwind233@outlook.com>
2023-02-08 11:05:44 +0800

    添加normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
+import "normalize.css"
 import { store } from "./store/index";
 import { Workbox } from "workbox-window";
 
